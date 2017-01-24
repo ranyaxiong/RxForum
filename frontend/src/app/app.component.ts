018ca6fa106768d80f44a12f6a72f2eec997d921
yaxiong <ryx-90@163.com>
2017-01-24 10:12:56 +0800

    Test semantic dropdown component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,15 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 
+declare var $: any;
 @Component({
   selector: 'app-root',
   providers: [AuthService],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   constructor(private auth: AuthService, private router: Router) {}
+  ngAfterViewInit() {
+   // $('.ui.dropdown').dropdown();
+  }
   logout() {
     this.auth.logout();
     this.router.navigate(['']);
